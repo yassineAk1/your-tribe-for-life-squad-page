1fd8fca89ca5147acaaba543d3d6700ce509be6c
Ikkezwam <mohamedelibrahymy374@gmail.com>
2026-09-02 15:02:15 +0200

    #3 HTML first

---
+++ squadpage/src/lib/card.svelte
@@ -0,0 +1,16 @@
+<script>
+
+ </script>
+
+
+<article>
+  <img src="https://i.imgur.com/o9fpo46_d.webp">
+  <a href="#">
+  <span>Naam</span>
+  <span>↗</span>
+</a>
+</article>
+
+<style>
+
+</style>
