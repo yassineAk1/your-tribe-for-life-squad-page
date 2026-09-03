a94bbb63ce1350f5a3ad034b1f5dac0f03a63ea0
Mohamed <mohamedelibrahymy374@gmail.com>
2026-09-03 12:42:43 +0200

    #3 SVG ipv content CSS

--- src/lib/card.svelte
+++ src/lib/card.svelte
@@ -7,11 +7,22 @@
   </picture>
   <a href="#">
     <h2>Naam</h2>
+    <svg
+      width="10"
+      height="10"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1.00001 14.6699L14.3914 1.27857M14.6704 13.275L14.3914 1.27857L2.39494 0.999588"
+        stroke="white"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
   </a>
 </article>
 
 <style>
-a::after {
-  content: '↗';
-}
-</style>
