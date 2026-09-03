e04fc8b88e9f4b13e2a51763fc55998ecf93e940
Mohamed <mohamedelibrahymy374@gmail.com>
2026-09-03 13:26:56 +0200

    #3 Data ophalen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,14 @@
 <script>
-    import Searchbar from "$lib/Searchbar.svelte";
-    import Card from "$lib/Card.svelte";
+  import Searchbar from "$lib/Searchbar.svelte";
+  import Card from "$lib/Card.svelte";
 
-    const { data } = $props();
-    let squad = data.squad[0];
+  const { data } = $props();
+  let squad = data.squad[0];
 </script>
 
 <Searchbar />
 
 {#each squad.persons as persons}
-    {@const person = persons.person_id}
-    <p>{person.name}</p>
+  {@const person = persons.person_id}
+  <p>{person.name}</p>
 {/each}
