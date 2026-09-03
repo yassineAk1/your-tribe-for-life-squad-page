bcab60f1cd54679df845cc11cb3d33a3795e6426
MathijsN <72303334+MathijsN@users.noreply.github.com>
2026-09-03 12:53:10 +0200

    searchbar styling and loading data #4

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,14 @@
 <script>
     import Searchbar from "$lib/Searchbar.svelte";
+
+    const { data } = $props();
+    let squad = data.squad[0];
+    console.log(squad);
 </script>
 
 <Searchbar />
+
+{#each squad.persons as persons}
+    {@const person = persons.person_id}
+    <p>{person.name}</p>
+{/each}
